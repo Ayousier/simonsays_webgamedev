64caf1bed8c00074368cf11d6cf12923f6bd0fab
Ayousier <johnsork@gmail.com>
2025-04-15 14:46:16 -0400

    Start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState, useEffect } from 'react';
+import ColorCard from "./components/ColorCard";
 
 function App() {
+
+  const [isOn, setIsOn] = useState(false);
+
+  const colorList = ["green", "red", "gold", "blue"];
+
+  const initPlay = {
+    isDisplay: false,
+    colors: [],
+    score: 0,
+    userPlay: false,
+    userColors: [],
+  };
+
+  const [play, setPlay] = useState(initPlay);
+
+  function startHandle() {
+    setIsOn(true);
+  }
+
+  useEffect(() => {
+    if(isOn){
+        setPlay({ ... initPlay, isDisplay: true});
+      } else {
+        setPlay(initPlay);
+      }
+  }, [isOn]);
+
+  useEffect(()=>{
+    if(isOn && play.isDisplay){
+      let newColor = colorList[Math.floor(Math.random()*4)]
+      const copyColors = [...play.colors];
+      copyColors.push(newColor);
+      setPlay({ ...play, colors: copyColors})
+    }
+  }, [isOn, play.isDisplay])
+
+  useEffect(()=> {
+    if(isOn && play.isDisplay && play.colors.length){
+      displayColors();
+    }
+  }, [isOn, play.isDisplay, play.colors.length])
+
+async function displayColors(){
+
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="simonDevice">
+          {colorList && 
+            colorList.map((v, i) => ( <ColorCard key={i} color={v}></ColorCard>))}
+        </div>
+        {!isOn && (
+          <button onClick={startHandle} className="startButton">
+            Start
+          </button>
+        )}
+        {isOn && (play.isDisplay || play.userPlay) && (
+          <div className= "score">{play.score}</div>
+        )}
       </header>
     </div>
   );
